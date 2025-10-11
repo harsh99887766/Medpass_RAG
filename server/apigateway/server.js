b751c0bd320033b770e1770354c87396ce191649
Parikshit09-coder <gurudesai2005@gmail.com>
2025-10-11 18:27:12 +0530

    Update server.js

--- server/apigateway/server.js
+++ server/apigateway/server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 require('dotenv').config();
 const PORT = process.env.PORT;
 
-
+//
 
 app.use('/node_server', proxy('https://deployment-production-4d20.up.railway.app'));
 app.use('/RAG_server', proxy('https://medpass-rag.onrender.com'));
